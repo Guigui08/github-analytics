538f73e6984be6346510e84685b8fbbfe72ac11f
guillaume <guillaume@wenabi.com>
2023-09-17 19:08:59 +0200

    add font awesome lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,9 +7,16 @@ import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-transla
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NavbarModule } from './layout/navbar/navbar.module';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { InitAppConfigurationService } from './shared/service/init-app-configuration.service';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+export function ConfigurationInit(initAppConfigurationService: InitAppConfigurationService) {
+  return (): Promise<void> => initAppConfigurationService.init();
 }
 
 @NgModule({
@@ -27,13 +34,22 @@ export function HttpLoaderFactory(http: HttpClient) {
       },
     }),
     NavbarModule,
+    FontAwesomeModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: ConfigurationInit,
+      deps: [InitAppConfigurationService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor(private readonly translateService: TranslateService) {
+  constructor(private readonly translateService: TranslateService, private readonly faLibrary: FaIconLibrary) {
     this.translateService.setDefaultLang('en');
     this.translateService.use('en');
+    this.faLibrary.addIconPacks(fas);
   }
 }
