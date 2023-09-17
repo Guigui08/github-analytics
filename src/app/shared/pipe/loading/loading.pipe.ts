76c9cb8eb345f5df07b9a79fa06e19093fad90e2
guillaume <guillaume@wenabi.com>
2023-09-17 11:44:15 +0200

    add loading pipe

---
+++ src/app/shared/pipe/loading/loading.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { catchError, isObservable, map, Observable, of, startWith } from 'rxjs';
+
+@Pipe({
+  name: 'loading',
+})
+export class LoadingPipe implements PipeTransform {
+  transform(val: Observable<any>, ...args: any[]): Observable<any> {
+    return isObservable(val)
+      ? val.pipe(
+          map((value: any) => ({
+            loading: value.type === 'start',
+            value: value.type ? value.value : value,
+          })),
+          startWith({ loading: true }),
+          catchError((error) => of({ loading: false, error }))
+        )
+      : val;
+  }
+}
